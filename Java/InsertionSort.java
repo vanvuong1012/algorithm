db3fb59db626cd817bd59a6557c68dab1ea0875f
thehau217vn <thehau217vn@gmail.com>
2020-12-15 22:39:12 +0700

    first commit java folder

---
+++ Java/InsertionSort.java
@@ -0,0 +1,33 @@
+package main;
+
+public class InsertionSort {
+	public void insertionSort(int arr[]) {
+		int n = arr.length;
+		for (int i = 1; i < n; ++i) {
+			int key = arr[i];
+			int j = i - 1;
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = key;
+		}
+	}
+
+	public static void exportArray(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 2, 4, 6, 8, 3, 5, 7, 9 };
+		InsertionSort sort = new InsertionSort();
+		sort.insertionSort(arr);
+		exportArray(arr);
+
+	}
+}
